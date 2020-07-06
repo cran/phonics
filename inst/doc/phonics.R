815092d8f6d79d839ff1525c2fa5fbd575866fed
James Howard <jh@jameshoward.us>
2020-07-06 03:40:04 +0000

    version 1.3.6

--- inst/doc/phonics.R
+++ inst/doc/phonics.R
@@ -1,4 +1,4 @@
-## ----basic-examples------------------------------------------------------
+## ----basic-examples-----------------------------------------------------------
 library("phonics")
 
 x1 <- "Catherine"
@@ -15,11 +15,11 @@ soundex(x)
 refinedSoundex(x1)
 refinedSoundex(x2)
 
-## ----soundex-test-example------------------------------------------------
+## ----soundex-test-example-----------------------------------------------------
 soundex(x1) == soundex(x2)
 soundex(x2) == soundex(x3)
 
-## ----mra-example---------------------------------------------------------
+## ----mra-example--------------------------------------------------------------
 (mra1 = mra_encode("Katherine"))
 (mra2 = mra_encode("Catherine"))
 (mra3 = mra_encode("Katarina"))
@@ -28,6 +28,6 @@ mra_compare(mra1, mra2)
 mra_compare(mra1, mra3)
 mra_compare(mra2, mra3)
 
-## ----mra-kw-example------------------------------------------------------
+## ----mra-kw-example-----------------------------------------------------------
 mra_compare(mra_encode("Catherine"), mra_encode("William"))
 
